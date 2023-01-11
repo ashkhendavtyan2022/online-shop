827cc6e955cb6179e4ea8332c9d655b303efeff1
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2023-01-11 08:55:33 +0400

    01-11-2023

--- react-app/src/Components/Pages/Main Page/Slider/index.jsx
+++ react-app/src/Components/Pages/Main Page/Slider/index.jsx
@@ -22,7 +22,7 @@ const Slider = () => {
     useEffect(() => {
         let slider = setInterval(() => {
           setIndex(index + 1);
-        }, 3000);
+        }, 5000);
         return () => clearInterval(slider);
       }, [index]);
 
